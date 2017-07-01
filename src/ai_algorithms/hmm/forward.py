8baaf15990e39ee664825277938875cf3cea7c40
Ramiro Camino <ramirocamino@gmail.com>
2017-07-01 18:32:03 +0200

    Forward algorithm. Name change in mini-forward algorithm for consistency.

---
+++ src/ai_algorithms/hmm/forward.py
@@ -0,0 +1,43 @@
+from ai_algorithms.markov_chains.mini_forward import mini_forward_step
+
+
+def observation_step(emission_probability, observation, probability):
+    """
+    Calculates the probability of every hidden state after an observation.
+    :param emission_probability: dictionary of dictionaries of probabilities for observations given states
+    :param observation: must be a hashable object
+    :param probability: dictionary of probabilities for every hidden state in the current time step
+    :return: dictionary of probabilities for every hidden state
+    """
+    p = {}
+    for x, p_x in probability.keys():
+        p[x] = p_x * emission_probability[observation][x]
+    return p
+
+
+def forward_step(transition_probability, emission_probability, observation, previous):
+    """
+    Calculates the probability of every hidden state after a time step and an observation.
+    :param transition_probability: dictionary of dictionaries of probabilities for states given previous states
+    :param emission_probability: dictionary of dictionaries of probabilities for observations given states
+    :param observation: must be a hashable object
+    :param previous: dictionary of probabilities for every hidden state in the previous time step
+    :return: dictionary of probabilities for every hidden state
+    """
+    p = mini_forward_step(transition_probability, previous)
+    return observation_step(emission_probability, observation, p)
+
+
+def forward(transition_probability, emission_probability, observations, prior):
+    """
+    Calculates the probability of every hidden state after a sequence of time steps and observations.
+    :param transition_probability: dictionary of dictionaries of probabilities for states given previous states
+    :param emission_probability: dictionary of dictionaries of probabilities for observations given states
+    :param observations: list of observations; they must be hashable objects
+    :param prior: dictionary of probabilities for the initial probability of every hidden state
+    :return: dictionary of probabilities for every hidden state
+    """
+    p = prior
+    for observation in observations:
+        p = forward_step(transition_probability, emission_probability, observation, p)
+    return p
--- src/ai_algorithms/markov_chains/mini_forward.py
+++ src/ai_algorithms/markov_chains/mini_forward.py
@@ -1,7 +1,7 @@
-def mini_forward_step(conditional_distribution, previous):
+def mini_forward_step(transition_probability, previous):
     """
     Calculates the probability distribution for every state in a Markov chain given the previous state distribution.
-    :param conditional_distribution: dictionary of dictionaries of probabilities for states given previous states
+    :param transition_probability: dictionary of dictionaries of probabilities for states given previous states
     :param previous: dictionary of probabilities for every state of the previous distribution
     :return: dictionary of probabilities for every state
     """
@@ -9,14 +9,14 @@ def mini_forward_step(conditional_distribution, previous):
     for x in previous.keys():
         p[x] = 0.0
         for x_previous, p_previous in previous.items():
-            p[x] += conditional_distribution[x][x_previous] * p_previous
+            p[x] += transition_probability[x][x_previous] * p_previous
     return p
 
 
-def mini_forward(conditional_distribution, prior, iterations=1000, tolerance=1e-6):
+def mini_forward(transition_probability, prior, iterations=1000, tolerance=1e-6):
     """
     Calculates the stationary probability distribution for every state in a Markov chain.
-    :param conditional_distribution: dictionary of dictionaries of probabilities for states given previous states
+    :param transition_probability: dictionary of dictionaries of probabilities for states given previous states
     :param prior: dictionary of probabilities for every state of the initial distribution
     :param iterations: maximum number of iterations
     :param tolerance: if the maximum difference between all the values is less than this number the algorithm stops
@@ -24,7 +24,7 @@ def mini_forward(conditional_distribution, prior, iterations=1000, tolerance=1e-
     """
     p = prior
     for iteration in xrange(iterations):
-        p_next = mini_forward_step(conditional_distribution, p)
+        p_next = mini_forward_step(transition_probability, p)
         max_change = 0.0
         for x in p.keys():
             max_change = max(max_change, abs(p_next[x] - p[x]))
