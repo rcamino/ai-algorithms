3e1d35b9602911cf5de799da3d5867861c566d79
Ramiro Camino <ramirocamino@gmail.com>
2017-07-03 00:28:05 +0200

    Moving topological ordering outside for future use.

---
+++ src/ai_algorithms/bayesian_networks/sampling/ordering.py
@@ -0,0 +1,24 @@
+import heapq
+import random
+
+
+def random_topological_order(root_nodes, random_state=None):
+    if random_state is None:
+        random_state = random.Random()
+    nodes = []
+    priority_queue = []
+    for root_node in root_nodes:
+        # priority is root level = 0
+        # break ties with random number
+        heapq.heappush(priority_queue, (0, random_state.random(), root_node))
+    visited = set()
+    while len(priority_queue) > 0:
+        level, _, node = heapq.heappop(priority_queue)
+        if node not in visited:
+            visited.add(node)
+            nodes.append(node)
+            for child in node.children:
+                # priority is parent level + 1
+                # break ties with random number
+                heapq.heappush(priority_queue, (level + 1, random_state.random(), child))
+    return nodes
--- src/ai_algorithms/bayesian_networks/sampling/prior.py
+++ src/ai_algorithms/bayesian_networks/sampling/prior.py
@@ -1,35 +1,9 @@
-import heapq
-import random
-
+from ai_algorithms.bayesian_networks.sampling.ordering import random_topological_order
 from ai_algorithms.probabilities.inference import observe
 from ai_algorithms.probabilities.sampling import sample_from_distribution
 
 
-def random_topological_order(root_nodes, random_state=None):
-    nodes = []
-    if random_state is None:
-        random_state = random.Random()
-    priority_queue = []
-    for root_node in root_nodes:
-        # priority is root level = 0
-        # break ties with random number
-        heapq.heappush(priority_queue, (0, random_state.random(), root_node))
-    visited = set()
-    while len(priority_queue) > 0:
-        level, _, node = heapq.heappop(priority_queue)
-        if node not in visited:
-            visited.add(node)
-            nodes.append(node)
-            for child in node.children:
-                # priority is parent level + 1
-                # break ties with random number
-                heapq.heappush(priority_queue, (level + 1, random_state.random(), child))
-    return nodes
-
-
 def prior_sampling(root_nodes, random_state=None):
-    if random_state is None:
-        random_state = random.Random()
     nodes = random_topological_order(root_nodes, random_state)
     values = []
     value_by_node = {}
