facf8fa478097d73d77f6f4c66b140b42ac200cd
Ramiro Camino <ramirocamino@gmail.com>
2017-07-03 00:29:08 +0200

    Should return samples with variable names.

--- src/ai_algorithms/bayesian_networks/sampling/prior.py
+++ src/ai_algorithms/bayesian_networks/sampling/prior.py
@@ -5,15 +5,13 @@ from ai_algorithms.probabilities.sampling import sample_from_distribution
 
 def prior_sampling(root_nodes, random_state=None):
     nodes = random_topological_order(root_nodes, random_state)
-    values = []
-    value_by_node = {}
+    value_by_name = {}
     for node in nodes:
         observations_by_name = {}
         for parent in node.parents:
-            observations_by_name[parent.name] = value_by_node[parent]
+            observations_by_name[parent.name] = value_by_name[parent.name]
         evidence = node.probability.create_evidence(observations_by_name)
         probability = observe(node.probability, evidence)
         value = sample_from_distribution(probability, random_state)
-        values.append(value)
-        value_by_node[node] = value
-    return values
+        value_by_name[node.name] = value
+    return value_by_name
