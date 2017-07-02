eae75e468e1139be138c1951bc39b0495b52f15c
Ramiro Camino <ramirocamino@gmail.com>
2017-07-03 00:48:10 +0200

    Abstraction between prior and rejection sampling.

--- src/ai_algorithms/bayesian_networks/sampling/prior.py
+++ src/ai_algorithms/bayesian_networks/sampling/prior.py
@@ -5,13 +5,13 @@ from ai_algorithms.probabilities.sampling import sample_from_distribution
 
 def prior_sampling(root_nodes, random_state=None):
     nodes = random_topological_order(root_nodes, random_state)
-    value_by_name = {}
+    observations_by_name = {}
     for node in nodes:
-        observations_by_name = {}
-        for parent in node.parents:
-            observations_by_name[parent.name] = value_by_name[parent.name]
-        evidence = node.probability.create_evidence(observations_by_name)
-        probability = observe(node.probability, evidence)
-        value = sample_from_distribution(probability, random_state)
-        value_by_name[node.name] = value
-    return value_by_name
+        observations_by_name[node.name] = prior_sampling_step(node, observations_by_name, random_state)
+    return observations_by_name
+
+
+def prior_sampling_step(node, observations_by_name, random_state):
+    evidence = node.probability.create_evidence(observations_by_name)
+    probability = observe(node.probability, evidence)
+    return sample_from_distribution(probability, random_state)
--- src/ai_algorithms/bayesian_networks/sampling/rejection.py
+++ src/ai_algorithms/bayesian_networks/sampling/rejection.py
@@ -1,6 +1,5 @@
 from ai_algorithms.bayesian_networks.sampling.ordering import random_topological_order
-from ai_algorithms.probabilities.inference import observe
-from ai_algorithms.probabilities.sampling import sample_from_distribution
+from ai_algorithms.bayesian_networks.sampling.prior import prior_sampling_step
 
 
 def rejection_sampling(root_nodes, observations_by_name, iterations=1000, random_state=None):
@@ -9,20 +8,15 @@ def rejection_sampling(root_nodes, observations_by_name, iterations=1000, random
     while repeat and iteration < iterations:
         repeat = False
         nodes = random_topological_order(root_nodes, random_state)
-        value_by_name = {}
+        new_observations_by_name = {}
         for node in nodes:
-            node_observations_by_name = {}
-            for parent in node.parents:
-                node_observations_by_name[parent.name] = value_by_name[parent.name]
-            evidence = node.probability.create_evidence(node_observations_by_name)
-            probability = observe(node.probability, evidence)
-            value = sample_from_distribution(probability, random_state)
+            value = prior_sampling_step(node, new_observations_by_name, random_state)
             if (node.name not in observations_by_name) or (observations_by_name[node.name] == value):
-                value_by_name[node.name] = value
+                new_observations_by_name[node.name] = value
             else:
                 repeat = True
                 iteration += 1
                 break
     if iteration >= iterations:
         raise Exception("Iteration limit reached.")
-    return value_by_name
+    return new_observations_by_name
